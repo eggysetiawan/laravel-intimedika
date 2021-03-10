179e1cfd7bdc6d6c883886f5f25535c3d89d825a
Rahmat Setiawan <setiawaneggy@gmail.com>
2021-03-10 19:01:19 +0700

    before isra miraj

--- app/Notifications/Offer/RevisionOfferNotification.php
+++ app/Notifications/Offer/RevisionOfferNotification.php
@@ -4,7 +4,6 @@ namespace App\Notifications\Offer;
 
 use App\Offer;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
