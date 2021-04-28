8753bd3c9949bf0a0702f0c225fffc41b0e6a69c
Rahmat Setiawan <setiawaneggy@gmail.com>
2021-04-28 15:09:16 +0700

    feat: add is_paid to taxes table

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,7 +1,9 @@
-<div class="row justify-content-center">
-    <div class="col-md-9">
-        <div class="card card-teal">
-            {{ $slot }}
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-9">
+            <div class="card card-teal">
+                {{ $slot }}
+            </div>
         </div>
     </div>
 </div>
