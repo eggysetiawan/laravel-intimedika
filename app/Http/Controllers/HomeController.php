0354077e6306e4898cca445a2236acbb6da7432c
kutamz123 <andikautama034@gmail.com>
2021-05-11 09:35:32 +0700

    fix: division zero

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -161,6 +161,13 @@ class HomeController extends Controller
             $charts[] = (new ChartService())->sales_chart($sales);
         }
 
-        return view('home', compact('chart1', 'charts'));
+        $user = auth()->user();
+        $name = $user->name;
+        $offers = $user->offers[0]->invoices[0]->orders[0]->price ?? '0';
+        $targets = $user->targets[0]->target ?? '0';
+
+        $percentage = $offers == 0 && $targets == 0 ? '0' : $offers / $targets * 100;
+
+        return view('home', compact('chart1', 'charts', 'name', 'offers', 'targets', 'percentage'));
     }
 }
