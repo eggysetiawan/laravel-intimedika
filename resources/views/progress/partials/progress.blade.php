baf5bddcf375ee6b53333ba93014e3765d311bbf
Rahmat Setiawan <setiawaneggy@gmail.com>
2021-05-11 14:35:16 +0700

    feat: blink on ready to approve

--- resources/views/progress/partials/progress.blade.php
+++ resources/views/progress/partials/progress.blade.php
@@ -29,42 +29,42 @@ switch ($progress) {
 
 @switch($offer->is_approved)
     @case(1)
-    @if ($progress <= 99)
-        <small>
-            <div class="justify-content-center">
-                <small class="text-secondary">{{ $offer->progress->status }}</small>
+        @if ($progress <= 99)
+            <small>
+                <div class="justify-content-center">
+                    <small class="text-secondary">{{ $offer->progress->status }}</small>
+                </div>
+            </small>
+        @endif
+        <div class="project_progress">
+            <div class="progress progress-sm">
+                <div class="progress-bar {{ $bar }} progress-bar-striped" role="progressbar"
+                    aria-volumenow="{{ $progress }}" aria-volumemin="0" aria-volumemax="100"
+                    style="width: {{ $progress }}%">
+                </div>
             </div>
-        </small>
-    @endif
-    <div class="project_progress">
-        <div class="progress progress-sm">
-            <div class="progress-bar {{ $bar }} progress-bar-striped" role="progressbar"
-                aria-volumenow="{{ $progress }}" aria-volumemin="0" aria-volumemax="100"
-                style="width: {{ $progress }}%">
-            </div>
-        </div>
-        <small>
-            {{ $progress }}% Completed.
+            <small>
+                {{ $progress }}% Completed.
 
-            @if ($progress == 99)
-                <a href="{{ route('invoices.toOrder', $offer->slug) }}"> <small class="badge badge-warning text-white"
-                        title="Approve Penawaran">Ready
-                        to
-                        Approve</small></a>
-            @endif
-        </small>
-    </div>
+                @if ($progress == 99)
+                    <a href="{{ route('invoices.toOrder', $offer->slug) }}"> <small
+                            class="badge badge-warning text-white rta" title="Approve Penawaran">Ready
+                            to
+                            Approve</small></a>
+                @endif
+            </small>
+        </div>
     @break
 
     @case(2)
-    <span class="badge badge-danger text-white">has been rejected.</span>
+        <span class="badge badge-danger text-white">has been rejected.</span>
     @break
 
     @case(3)
-    <span class="badge badge-secondary text-white">On Hold.</span>
+        <span class="badge badge-secondary text-white">On Hold.</span>
 
     @break
 
     @default
-    <span class="badge badge-warning text-white">Ready to Approve.</span>
+        <span class="badge badge-warning text-white rta">Ready to Approve.</span>
 @endswitch
