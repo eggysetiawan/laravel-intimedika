97435b9a4544707bd293dee961ca998a0faaadc1
Rahmat Setiawan <setiawaneggy@gmail.com>
2021-06-13 20:21:32 +0700

    fix: remove delay loading

--- public/js/pre.js
+++ public/js/pre.js
@@ -3,8 +3,8 @@ var loader = document.querySelector(".loader");
 window.addEventListener("load", vanish);
 
 function vanish() {
-    // loader.classList.add("disppear");
-    setTimeout(function() {
-        loader.classList.add("disppear");
-    }, 2665);
+    loader.classList.add("disppear");
+    // setTimeout(function() {
+    //     loader.classList.add("disppear");
+    // }, 2665);
 }
