d86556851592e7701a80a905416e6e00e20d5167
Rahmat Setiawan <setiawaneggy@gmail.com>
2021-07-22 16:11:26 +0700

    feat: FormSparepart mrcs

--- app/FormSparepart.php
+++ app/FormSparepart.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class FormAccessories extends Model
+class FormSparepart extends Model
 {
     protected $fillable = [
         'modality_id',
---
+++ app/Http/Controllers/FormSparepartController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\FormSparepart;
+use Illuminate\Http\Request;
+
+class FormSparepartController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\FormSparepart  $formSparepart
+     * @return \Illuminate\Http\Response
+     */
+    public function show(FormSparepart $formSparepart)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\FormSparepart  $formSparepart
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(FormSparepart $formSparepart)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\FormSparepart  $formSparepart
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, FormSparepart $formSparepart)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\FormSparepart  $formSparepart
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(FormSparepart $formSparepart)
+    {
+        //
+    }
+}
