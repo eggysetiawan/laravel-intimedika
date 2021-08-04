4122f6f0552f0680ca9c71506701d51abdf5eabf
Rahmat Setiawan <setiawaneggy@gmail.com>
2021-08-04 11:28:46 +0700

    feat: Activity Media Seeder

--- database/seeds/ServiceMediaSeeder.php
+++ database/seeds/ServiceMediaSeeder.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Storage;
 class ServiceMediaSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Run the database seeds.s
      *
      * @return void
      */
