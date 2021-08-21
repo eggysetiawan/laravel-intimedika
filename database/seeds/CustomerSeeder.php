9e0e3482eee22ebb7a959e6600bb0f4ec8475087
Rahmat Setiawan <setiawaneggy@gmail.com>
2021-08-21 18:06:33 +0700

    fix: customer modality factory

--- database/seeds/CustomerSeeder.php
+++ database/seeds/CustomerSeeder.php
@@ -17,45 +17,47 @@ class CustomerSeeder extends Seeder
      */
     public function run()
     {
-        // factory(Customer::class, 50)->create();
-        $sales_kunjungan = SalesKunjungan::query()
-            ->whereNotNull('fk_rs')
-            ->get();
-
-        foreach ($sales_kunjungan as $kunjungan) {
-            $username = User::where('username', $kunjungan->username)->first()->id;
-            $hospital = Hospital::where('name', $kunjungan->rs_kunjungan)->first()->id;
-
-            if ($kunjungan->email_kunjungan == '') {
-                $kunjungan->email_kunjungan = null;
-            }
+        factory(Customer::class, 50)->create();
 
 
+        // $sales_kunjungan = SalesKunjungan::query()
+        //     ->whereNotNull('fk_rs')
+        //     ->get();
 
-            $customers = Customer::create([
-                'user_id' => $username,
-                'slug' => Str::slug(uniqid('cust') . '-' . $kunjungan->rs_kunjungan),
-                'name' => $kunjungan->nama_kunjungan,
-                'person_in_charge' => $kunjungan->nama_kunjungan,
-                'mobile' => $kunjungan->hp_kunjungan,
-                'role' => $kunjungan->jabatan_kunjungan,
-                'city' => $kunjungan->kota_kunjungan,
-                'address' => $kunjungan->alamat_kunjungan,
-                'email' => $kunjungan->email_kunjungan,
+        // foreach ($sales_kunjungan as $kunjungan) {
+        //     $username = User::where('username', $kunjungan->username)->first()->id;
+        //     $hospital = Hospital::where('name', $kunjungan->rs_kunjungan)->first()->id;
 
-            ]);
-            $customers->hospitals()->attach($hospital);
+        //     if ($kunjungan->email_kunjungan == '') {
+        //         $kunjungan->email_kunjungan = null;
+        //     }
 
-            Visit::create([
-                'customer_id' => $customers->id,
-                'user_id' => $username,
-                'slug' => Str::slug(uniqid('visit') . '-' . $kunjungan->request),
-                'request' => $kunjungan->req_kunjungan,
-                'result' => $kunjungan->hasil_kunjungan,
-                'is_visited' => 1,
-                'created_at' => $kunjungan->now_kunjungan,
-                'updated_at' => $kunjungan->now_kunjungan,
-            ]);
-        }
+
+
+        //     $customers = Customer::create([
+        //         'user_id' => $username,
+        //         'slug' => Str::slug(uniqid('cust') . '-' . $kunjungan->rs_kunjungan),
+        //         'name' => $kunjungan->nama_kunjungan,
+        //         'person_in_charge' => $kunjungan->nama_kunjungan,
+        //         'mobile' => $kunjungan->hp_kunjungan,
+        //         'role' => $kunjungan->jabatan_kunjungan,
+        //         'city' => $kunjungan->kota_kunjungan,
+        //         'address' => $kunjungan->alamat_kunjungan,
+        //         'email' => $kunjungan->email_kunjungan,
+
+        //     ]);
+        //     $customers->hospitals()->attach($hospital);
+
+        //     Visit::create([
+        //         'customer_id' => $customers->id,
+        //         'user_id' => $username,
+        //         'slug' => Str::slug(uniqid('visit') . '-' . $kunjungan->request),
+        //         'request' => $kunjungan->req_kunjungan,
+        //         'result' => $kunjungan->hasil_kunjungan,
+        //         'is_visited' => 1,
+        //         'created_at' => $kunjungan->now_kunjungan,
+        //         'updated_at' => $kunjungan->now_kunjungan,
+        //     ]);
+        // }
     }
 }
